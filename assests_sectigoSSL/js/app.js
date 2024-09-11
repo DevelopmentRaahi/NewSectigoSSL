79d59048339920183aeb3e76bf77f4b022f1767d
developertool45 <developertool45@gmail.com>
2024-09-11 17:08:32 +0530

    update

--- assests_sectigoSSL/js/app.js
+++ assests_sectigoSSL/js/app.js
@@ -25,28 +25,24 @@
 //   document.body.scrollTop = 0;
 //   document.documentElement.scrollTop = 0;
 // }
-let mybutton = document.getElementById("btn-back-to-top");
+const mybutton = document.getElementById("btn-back-to-top");
 let socialIcons = document.querySelector(".whatsap_button");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-  scrollFunction();
-};
-
+if(mybutton){
 function scrollFunction() {
-  if (
-    document.body.scrollTop > 10 ||
-    document.documentElement.scrollTop > 10
-  ) {
-    socialIcons.style.display = "block";
-    mybutton.style.display = "block";
-  
+  if ( document.documentElement.scrollTop > 10  ) {
+        socialIcons.style.display = "block";
+       mybutton.classList.add('show')  
   } else {
-    mybutton.style.display = "none";
-    socialIcons.style.display = "none";
-    
-  }
+       socialIcons.style.display = "none";
+        mybutton.classList.remove('show')  
+    }
 }
+
+window.addEventListener("scroll", scrollFunction)
+}
+
 // When the user clicks on the button, scroll to the top of the document
 mybutton.addEventListener("click", backToTop);
 
